84debd29c7fc9d07dc47e184f738b87290917329
steven_q <qichanggui@huawei.com>
2022-02-23 14:21:35 +0800

    fix repeat load policy in init
    Signed-off-by: steven_q <qichanggui@huawei.com>
    Change-Id: I3f12f447edfa3ea7bf1ad79817c5dfe767330907

--- interfaces/policycoreutils/src/load_policy.c
+++ interfaces/policycoreutils/src/load_policy.c
@@ -21,20 +21,11 @@
 int LoadPolicy(void)
 {
     int ret, enforce = 0;
-
     if ((ret = selinux_init_load_policy(&enforce))) {
         if (enforce > 0) {
-            fprintf(stderr,
-                    "Can't load policy and enforcing mode requested:  %s\n",
-                    strerror(errno));
+            fprintf(stderr, "Can't load policy and enforcing mode requested:  %s\n", strerror(errno));
             return -1;
         }
     }
-
-    if ((ret = selinux_mkload_policy(0)) < 0) {
-        fprintf(stderr, "Can't load policy:  %s\n", strerror(errno));
-        return -1;
-    }
-
     return 1;
 }
