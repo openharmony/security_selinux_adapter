ba5306036804dbf6bc1648e05cc04a9f862c9428
limerence <shituanhui@huawei.com>
2023-10-11 17:19:30 +0800

    modify devloper proc value
    Signed-off-by: limerence <shituanhui@huawei.com>

--- interfaces/policycoreutils/src/load_policy.cpp
+++ interfaces/policycoreutils/src/load_policy.cpp
@@ -371,7 +371,7 @@ static bool IsDeveloperMode()
         return false;
     }
     selinux_log(SELINUX_WARNING, "Get developer mode, %s\n", devMode.c_str());
-    return devMode == "1";
+    return devMode == "const.security.developermode=1";
 #else
     return false;
 #endif
