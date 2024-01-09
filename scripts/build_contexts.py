8f681e10f5fdf0fe53361be4072c3d1e46a6f5cb
yeyuning <yeyuning2@huawei.com>
2024-01-09 15:51:20 +0800

    代码清理告警
    Signed-off-by: yeyuning <yeyuning2@huawei.com>
    Change-Id: I0d0eb17ba3ebed6eb095617ea8fdec69ef553f23

--- scripts/build_contexts.py
+++ scripts/build_contexts.py
@@ -58,14 +58,15 @@ def traverse_folder_in_type(search_dir_list, file_suffix):
     """
     flag = 0
     policy_file_list = []
+
     for item in search_dir_list:
-        for root, _, files in os.walk(item):
-            for each_file in files:
-                file_name = os.path.basename(each_file)
-                if file_name == file_suffix:
-                    file_list_path = os.path.join(root, each_file)
-                    flag |= check_contexts_file(file_list_path)
-                    policy_file_list.append(file_list_path)
+        for root, _, files in sorted(os.walk(item)):
+            filtered_files = [f for f in files if f.endswith(file_suffix)]
+            for each_file in filtered_files:
+                file_list_path = os.path.join(root, each_file)
+                flag |= check_contexts_file(file_list_path)
+                policy_file_list.append(file_list_path)
+
     if flag:
         raise Exception(flag)
     policy_file_list.sort()
@@ -179,6 +180,25 @@ def echo_error():
     print("***********************************************************")
 
 
+def sehap_process_line(line, line_index, contexts_write, domain, contexts_file):
+    line_ = line.lstrip()
+    if line_.startswith('#') or line_.strip() == '':
+        contexts_write.write(line)
+        return
+
+    pattern = re.compile(
+        r'apl=(system_core|system_basic|normal)\s+((name|debuggable)=\S+\s+)?domain=(\S+)\s+type=(\S+)\s*\n')
+    match = pattern.match(line_)
+    if match:
+        if domain:
+            line = match.group(1) + " u:r:" + match.group(4) + ":s0\n"
+        else:
+            line = match.group(1) + " u:object_r:" + match.group(5) + ":s0\n"
+        contexts_write.write(line)
+    else:
+        print(contexts_file + ":" + str(line_index) + " format check fail")
+        raise Exception(1)
+
 def check_sehap_contexts(args, contexts_file, domain):
     """
     check domain or type defined in sehap_contexts.
@@ -188,45 +208,31 @@ def check_sehap_contexts(args, contexts_file, domain):
     :return:
     """
     shutil.copyfile(contexts_file, contexts_file + "_bk")
-    err = 0
-    with open(contexts_file + "_bk", 'r') as contexts_read, open(contexts_file, 'w') as contexts_write:
-        pattern = re.compile(
-            r'apl=(system_core|system_basic|normal)\s+((name|debuggable)=\S+\s+)?domain=(\S+)\s+type=(\S+)\s*\n')
-        line_index = 0
-        for line in contexts_read:
-            line_ = line.lstrip()
-            line_index += 1
-            if line_.startswith('#') or line_.strip() == '':
-                contexts_write.write(line)
-                continue
-            match = pattern.match(line_)
-            if match:
-                if domain:
-                    line = match.group(1) + " u:r:" + match.group(4) + ":s0\n"
-                else:
-                    line = match.group(1) + " u:object_r:" + \
-                        match.group(5) + ":s0\n"
-                contexts_write.write(line)
-            else:
-                print(contexts_file + ":" +
-                      str(line_index) + " format check fail")
-                err = 1
-    if err:
+    try:
+        with open(contexts_file + "_bk", 'r') as contexts_read, open(contexts_file, 'w') as contexts_write:
+            line_index = 0
+            for line in contexts_read:
+                line_index += 1
+                sehap_process_line(line, line_index, contexts_write, domain, contexts_file)
+    except Exception as e:
         shutil.move(contexts_file + "_bk", contexts_file)
         echo_error()
-        raise Exception(err)
+        raise e
+
     check_cmd = [os.path.join(args.tool_path, "sefcontext_compile"),
                  "-o", contexts_file + ".bin",
                  "-p", args.policy_file,
                  contexts_file]
     ret = subprocess.run(" ".join(check_cmd), shell=True).returncode
+
     if ret != 0:
         shutil.move(contexts_file + "_bk", contexts_file)
         raise Exception(ret)
+
     shutil.move(contexts_file + "_bk", contexts_file)
+
     if os.path.exists(contexts_file + ".bin"):
         os.unlink(contexts_file + ".bin")
-
 
 def build_file_contexts(args, output_path, policy_path, all_policy_path):
     if args.components == "default":
@@ -276,7 +282,8 @@ def build_sehap_contexts(args, output_path, policy_path):
 
 
 def prepare_build_path(dir_list, root_dir, build_dir_list):
-    build_contexts_list = ["base/security/selinux_adapter/sepolicy/base", "base/security/selinux_adapter/sepolicy/ohos_policy"]
+    build_contexts_list = \
+        ["base/security/selinux_adapter/sepolicy/base", "base/security/selinux_adapter/sepolicy/ohos_policy"]
     build_contexts_list += dir_list.split(":")
 
     for i in build_contexts_list:
@@ -292,7 +299,7 @@ def prepare_build_path(dir_list, root_dir, build_dir_list):
 
 def traverse_folder_in_dir_name(search_dir, folder_suffix):
     folder_list = []
-    for root, dirs, _ in os.walk(search_dir):
+    for root, dirs, _ in sorted(os.walk(search_dir)):
         for dir_i in dirs:
             if dir_i == folder_suffix:
                 folder_list.append(os.path.join(root, dir_i))
